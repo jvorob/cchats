36d533811632e07656641c9db5b5dc62b96606ec
Justin Kim <yulli@yulli.org>
2014-12-07 17:54:40 -0500

    I'm not sure what I did, but I'm committing so I can work.

--- client.c
+++ client.c
@@ -1,22 +1,35 @@
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 
 #include "util.h"
 #include "client.h"
 
 int start_client() {
+        const int PORT = 10000;
+        char *port_string;
+        sprintf(port_string, "%d", PORT);
+
         printf("Starting client\n");
 
         // Open connection to server here
+        struct addrinfo hints, *res, *res0;
+        hints.ai_family = PF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        memset(&hints, 0, sizeof(hints));
 
-        const int bufsize = 1024;
-        char *buf = malloc(bufsize);
+        int error = getaddrinfo("107.170.106.89", port_string, &hints, &res0);
+
+        const int BUF_SIZE = 1024;
+        char *buf = malloc(BUF_SIZE);
 
         while(1) {
                 print_prompt();
 
-                if (_getline(buf, bufsize) == -1)
-                        printf("Command too long, cannot be more than %d chars\n", bufsize);
+                if (_getline(buf, BUF_SIZE) == -1)
+                        printf("Command too long, cannot be more than %d chars\n", BUF_SIZE);
 
                 send_to_server(buf);
         }
